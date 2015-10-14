7d5d8b5bb799a99703d6b6c20aa51c2acbeb6dec
sqatlab <sqatlab@gmail.com>
2015-10-14 14:09:09 +0300

    First test added

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -3,6 +3,7 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.BowlingGame;
 
 public class TestBowlingScoreCalculator {
 
